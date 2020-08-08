08a990ded0540daf71a0d3bbdd25475e47cc44e6
Avicena13 <68338411+Avicena13@users.noreply.github.com>
2020-08-07 20:30:48 -0400

    Add files via upload

---
+++ Piedra y Papel.js
@@ -0,0 +1,10 @@
+var Piedra = 1;
+var Papel = 2; 
+var Tijera = 3; 
+
+function Game (Piedra, Papel){
+    if (Piedra <  Papel ) {console.log("Gana Papel");}
+    else  {console.log("Empate");}
+ } 
+
+ 
