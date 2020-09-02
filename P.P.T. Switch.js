722caebe9612407a02370cb20a0027595c3cf8a3
Avicena13 <68338411+Avicena13@users.noreply.github.com>
2020-09-02 00:36:16 -0400

    Add files via upload

---
+++ P.P.T. Switch.js
@@ -0,0 +1,23 @@
+var op1 = "Tijera";
+var op2 = "Papel";
+var op3 = "Piedra";
+
+function play (player, ai){ 
+
+    switch (true) {
+    case (player === op1 && ai === op2): console.log ("Gana player")
+        break;
+    case (player === op2 && ai === op1): console.log ("Gana ai")
+        break;
+    case (player === op2 && ai === op3): console.log ("Gana player")
+        break;
+    case (player === op3 && ai === op2): console.log ("Gana ai")
+        break;
+    case (player === op1 && ai === op3): console.log ("Gana ai")
+        break;
+    case (player === op3 && ai === op1): console.log ("Gana player")
+        break; 
+    default: console.log ("Empate")
+        break; }
+}
+
